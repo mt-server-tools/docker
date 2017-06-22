90e0901fba50a418401d11afce45b4e6c565598b
Tai @ Flex <dch.tai@gmail.com>
2017-06-23 00:11:25 +0100

    moved rundmine

--- run-dmine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mtdatadir="$PWD/minetest-data"
-
-mkdir -p "$mtdatadir"
-mkdir -p "$mtdatadir/world"
-mkdir -p "$mtdatadir/mods"
-
-docker run -d -v "$mtdatadir:/root/minetest/userdata:rw" "$@"
